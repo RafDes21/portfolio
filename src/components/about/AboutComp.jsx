cdb7c219b58de21d92909ac088ef0cd42921b406
RafDes21 <israeltineo84@gmail.com>
2022-06-09 14:50:11 -0300

    actualizamos AboutComp

--- src/components/about/AboutComp.jsx
+++ src/components/about/AboutComp.jsx
@@ -10,10 +10,10 @@ const AboutComp = () => {
          <Card.Body>
          <Card.Title>Israel Tineo</Card.Title>
          <Card.Text>
-         Ya hace como diez años radico en Argentina, soy de nacionalidad Peruana. Mi desición por el mundo del desarrollo empezó
-         con un curso de programación inical, a lo que me encanto y recuerdo hasta el día de hoy una frase de mi profesor en el primer día de clase <i>"la programación es resolver los problemas de los demás"</i>,
-         luego terminé mi formación como <b>Front-End usando como libreria React</b>. Aparte, mis pasatiempos es jugar al ajedrez, leer libros, y hacer deporte.
-          <i><strong> Me gusta mucho enseñar a los demás,</strong> </i>lo que voy aprendiendo, también aprovecho ayudar y colaborar por vía de grupos de de redes (Whatsapp), y facebook. Ya que estoy en varios grupos de estos.
+          Mi camino por el mundo del desarrollo empezó
+         con un curso de programación inical,  lo cual me encanto y recuerdo hasta el día de hoy una frase de mi profesor en el primer día de clase <i>"la programación es resolver los problemas de los demás"</i>.
+         Posteriormente decidí preparame en las tecnologias mas demandadas por la industria. Me formé como <b>Desarrollador Front-End usando como libreria React.Js</b>, y siguiendo el camino, actualmente estoy capacitandome como Back-end, con el objetivo de volverme Full Stack. Aparte, mis pasatiempos es jugar al ajedrez, leer libros, y hacer deporte.
+          <i><strong> Me agrada mucho enseñar a los demás</strong> </i>lo que voy aprendiendo, también aprovecho ayudar y colaborar por vía de grupos de redes (Whatsapp y facebook).
         </Card.Text>
         <Button variant="primary"> Descargar CV</Button>
         </Card.Body>
