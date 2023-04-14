c572574abc2cd918e4725cea3f7c52c5502c57d6
unknown <israeltineo84@gmail.com>
2023-04-14 16:19:46 -0300

    [features]add project a folder db

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -7,11 +7,11 @@ export const PROJECTS = [
         tools:[9, 10, 11, 4]
     },
     {
-        title: "Clone-Netflix",
-        gitHub: "https://github.com/RafDes21/clonNetflix.git",
-        web:"https://clon-netflix-rafdes21.vercel.app/",
-        image:"https://images.macrumors.com/t/YHEUTAPq16J7TOmZ6FwxlNvkNmI=/1920x/article-new/2021/11/netflix-games-launch.jpg",
-        tools:[9, 10, 11, 4]
+        title: "Bar-El Taller",
+        gitHub: "https://github.com/RafDes21/taller-cervezeria.git",
+        web:"https://taller-cervezeria.vercel.app/",
+        image:"https://media-cdn.tripadvisor.com/media/photo-s/1d/4f/a6/00/barra-con-doce-tiradores.jpg",
+        tools:[1, 4, 11]
     },
     {
         title: "Clone-Netflix",
