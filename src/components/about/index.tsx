cb7e8148a04e79e5e5f9852211543d0d29bb9dc5
RafDes21 <israeltineo84@gmail.com>
2023-07-29 18:34:49 -0300

    add image

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -3,34 +3,31 @@ import pdf from "../../pdf/CVIsra.pdf";
 import { FaHandPointRight } from "react-icons/fa";
 import { BsCloudArrowDownFill } from "react-icons/bs";
 import { Animation } from "./Animation";
+import imageAbout from "../../assets/android.jpg";
 
 const About = () => {
   return (
     <div className="about-container">
       <div className="about">
-        <h3>Hola, me llamo Israel...</h3>
-        <h2>DESARROLLADOR DE SOFTWARE</h2>
-        <p>
-          Actualmente radico en Argentina,
-          <br /> desde el 2020 he estado capacitandome de manera autodidacta y
-          cursos,
-          <br /> tengo conocimientos en <b>lógica de programación</b>, luego me
-          preparé con <b>Javascript</b>,
-          <br /> manejando en el frontend la libreria de <b>React.js</b> y{" "}
-          <b>React-Native para App Mobile</b> y en el Backend <b>Node.js</b>{" "}
-          como entorno de desarrollo de Javascript.
-          <br />
-          agrego también el manejo de <b>Base de Datos</b> como MongoDB Y MysQL.
-        </p>
-        <div className="cv">
-          <span>
-            <a href={pdf} download>
-              <FaHandPointRight />
-              <span className="download"> Download CV</span>
-              <BsCloudArrowDownFill />
-            </a>
-          </span>
+        <div className="about-content-1">
+          <h3>Hola, me llamo Israel...</h3>
+          <h2>DESARROLLADOR DE SOFTWARE</h2>
+          <div className="cv">
+            <span>
+              <a href={pdf} download>
+                <FaHandPointRight />
+                <span className="download"> Download CV</span>
+                <BsCloudArrowDownFill />
+              </a>
+            </span>
+          </div>
         </div>
+        <div className="about-content-2">
+          <div>
+            <img src={imageAbout} />
+          </div>
+        </div>
+
         <Animation />
       </div>
     </div>
